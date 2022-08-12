031fda75a5f1ccee1dc02851794bc0c3c0eb76c4
Elliot Courant <elliot@treasuryprime.com>
2022-08-12 16:45:17 -0500

    chore: Messing with telescope.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -2,6 +2,11 @@
 -- This sorts by recent usage for the buffer window and excludes the current
 -- buffer to achieve this behavior.
 require('telescope').setup{
+    defaults = {
+      layout_config = {
+        vertical = { width = 0.75 }
+      },
+    },
     pickers = {
         buffers = {
             ignore_current_buffer = true,
