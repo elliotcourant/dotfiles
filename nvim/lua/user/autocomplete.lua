20ba7afaf798c690371044f0c3fa744cd122ebfd
Elliot Courant <elliot@treasuryprime.com>
2023-07-17 09:45:15 -0500

    chore: Fixing virtual diagnostic.

--- nvim/lua/user/autocomplete.lua
+++ nvim/lua/user/autocomplete.lua
@@ -186,10 +186,10 @@ require('lspconfig')['gopls'].setup {
     }
   },
   handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = true,
-      virtual_lines = false,
-    }),
+    -- ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    --   virtual_text = true,
+    --   virtual_lines = false,
+    -- }),
   }
 }
 
