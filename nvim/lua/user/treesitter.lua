60241a82b3e250c0cdc446d94e2967366fb94a6e
Elliot Courant <elliot@treasuryprime.com>
2024-03-14 09:52:42 -0500

    chore: Even more clojure syntax highlighting

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -43,16 +43,53 @@ require 'nvim-treesitter.configs'.setup {
   },
 };
 
+vim.cmd [[
+" Color Palette
+" This initial color is the background i believe.
+" if g:material_theme_style == 'default'
+"   let s:gray1 = '#263238'
+" elseif g:material_theme_style == 'palenight'
+"   let s:gray1 = '#292D3E'
+" elseif g:material_theme_style == 'dark'
+"   let s:gray1 = '#212121'
+" endif
+" 
+" let s:gray2 = g:material_theme_style == 'dark' ? '#292929' : '#2c3a41'
+" let s:gray3 = g:material_theme_style == 'dark' ? '#474646' : '#425762'
+" let s:gray4 = g:material_theme_style == 'dark' ? '#6a6c6c' : '#658494'
+" let s:gray5 = g:material_theme_style == 'dark' ? '#f1f2f3' : '#aebbc5'
+" " let s:gray5 = g:material_theme_style == 'dark' ? '#b7bdc0' : '#aebbc5'
+" let s:red = '#ff5370'
+" let s:green = '#c3e88d'
+" let s:yellow = '#ffcb6b'
+" let s:blue = '#82aaff'
+" let s:purple = '#c792ea'
+" let s:cyan = '#89ddff'
+" let s:orange = '#f78c6c'
+" let s:indigo = '#BB80B3'
+]]
+
+local white = '#f1f2f3';
+local gray5   = '#b7bdc0';
+local red     = '#ff5370';
+local red1    = '#F07178';
+local green   = '#c3e88d';
+local yellow  = '#ffcb6b';
+local purple = '#c792ea';
+local cyan    = '#89ddff';
+local orannge = '#f78c6c';
+
 -- Fix clojure macros not highlighting properly. Like defn or when
-vim.api.nvim_set_hl(0, "@lsp.type.macro.clojure", { link = "Statement" })
-vim.api.nvim_set_hl(0, "@lsp.type.event.clojure", { link = "@tag.attribute" })
-vim.api.nvim_set_hl(0, "@variable",               { fg   = "#FF5370"   })
-vim.api.nvim_set_hl(0, "@symbol",                 { fg   = "#b7bdc0"   })
-vim.api.nvim_set_hl(0, "@parameter",              { fg   = "#F78C6C"   })
-vim.api.nvim_set_hl(0, "@tag",                    { fg   = "#F07178"   })
-vim.api.nvim_set_hl(0, "@tag.attribute",          { fg   = "#FFCB6B"   })
-vim.api.nvim_set_hl(0, "@namespace",              { fg   = "#FFCB6B"   })
-vim.api.nvim_set_hl(0, "@type",                   { fg   = "#C3E88D"   })
+vim.api.nvim_set_hl(0, "@lsp.type.macro.clojure", { fg = purple  })
+vim.api.nvim_set_hl(0, "@lsp.type.event.clojure", { fg = white   })
+vim.api.nvim_set_hl(0, "@lsp.type.type.clojure",  { fg = cyan    })
+vim.api.nvim_set_hl(0, "@variable",               { fg = red     })
+vim.api.nvim_set_hl(0, "@symbol",                 { fg = gray5   })
+vim.api.nvim_set_hl(0, "@parameter",              { fg = orannge })
+vim.api.nvim_set_hl(0, "@tag",                    { fg = red1    })
+vim.api.nvim_set_hl(0, "@tag.attribute",          { fg = yellow  })
+vim.api.nvim_set_hl(0, "@namespace",              { fg = yellow  })
+vim.api.nvim_set_hl(0, "@type",                   { fg = green   })
 
 function InspectHighlight()
   local result = vim.treesitter.get_captures_at_cursor(0)
