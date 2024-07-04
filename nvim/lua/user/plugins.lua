468dd743a9994b8bdd6ea86d2f2913ae6e77edeb
Elliot Courant <me@elliotcourant.dev>
2024-07-04 09:23:36 -0500

    chore: Fixing gitsigns warning

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -298,12 +298,21 @@ return packer.startup(function(use)
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup {
-        signs                        = {
-          add          = { hl = 'GitSignsAdd', text = '│', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-          change       = { hl = 'GitSignsChange', text = '│', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-          delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-          topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-          changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signs_staged                 = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
         },
         signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
         numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -333,9 +342,6 @@ return packer.startup(function(use)
           relative = 'cursor',
           row      = 0,
           col      = 1
-        },
-        yadm                         = {
-          enable = false
         },
       }
     end
